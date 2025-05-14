d5635549006d811bf3f221bb56b930dae1890fc5
kaketo <kaketo.suwageeks@gmail.com>
2025-05-15 07:32:36 +0900

    self_introductionの内容を移行

--- react_self_intoroduction/src/App.tsx
+++ react_self_intoroduction/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,40 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+          <h1>自己紹介</h1>
+      </header>
+      <main>
+          <article>
+              <h2>基本情報</h2>
+              <table>
+                  <tr><td>名前</td><td>金子健人</td></tr>
+                  <tr><td>所属</td><td>公立諏訪東京理科大学</td></tr>
+                  <tr><td>学籍番号</td><td>T223035</td></tr>
+                  <tr><td>出身地</td><td>長野県諏訪市</td></tr>
+              </table>
+              <h2>地域連携課題演習で取り組んだ内容</h2>
+              <p>デジタルホワイトボードを活用した非共有ノウハウの活用</p>
+              <details>
+                  <summary>詳細を表示</summary>
+                  <p>電子ホワイトボードにアイディアを書き込んだ時，過去のアイディアを表示するサービスの開発をおこなった．</p>
+                  <ul>
+                      <li><strong>FPLの課題</strong>：ヒューマンエラーによって不良が生じていた．</li>
+                      <li><strong>ヒューマンエラーについて</strong>：個人的要因，環境的要因など原因が多岐にわたる．</li>
+                      <li><strong>現状の対策</strong>：ホワイトボードを用いてブレインストーミングをしながら，それぞれの解決方法を検討していた．</li>
+                      <li><strong>見出した問題点</strong>：検討内容がアナログで保存されており参照しずらく，類似議題について検討する際も一から検討する必要があった．</li>
+                      <li><strong>提案した改善案</strong>：電子ホワイトボードに議題等を書き込んだ時，過去のアイディアを表示することで議論の効率化をはかった．</li>
+                  </ul>
+              </details>
+              <h2>VitaLinkで目指すこと</h2>
+                  <p>バックエンド・フロントエンドの技術力向上，また課題発見・解決能力向上</p>
+              <h2>質問フォーム</h2>
+              <form>
+                  <textarea rows={4} cols={40} placeholder="ここに質問を入力してください"></textarea>
+                  <button type="submit">送信</button>
+              </form>
+      </article>
+      </main>
     </>
   )
 }
